a9c24e573e5633ee41e21c59024f65ea84c23848
sushaanttb <sushantbhalla@ymail.com>
2020-07-09 15:05:07 +0530

    Added the Builder annotation

--- src/main/java/com/example/moviebookingdemo/coreapi/dto/TicketDTO.java
+++ src/main/java/com/example/moviebookingdemo/coreapi/dto/TicketDTO.java
@@ -1,11 +1,13 @@
 package com.example.moviebookingdemo.coreapi.dto;
 
 import com.example.moviebookingdemo.coreapi.MovieSlot;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@Builder
 public class TicketDTO {
 
     String id;
