5c9575fa3f876172ec6bf7ce0e89b58b282a5e0a
sushaanttb <sushantbhalla@ymail.com>
2020-07-13 16:49:51 +0530

    Added Serialization/Deserialization based on Enum Value.

--- src/main/java/com/example/moviebookingdemo/coreapi/MovieSlot.java
+++ src/main/java/com/example/moviebookingdemo/coreapi/MovieSlot.java
@@ -1,5 +1,20 @@
 package com.example.moviebookingdemo.coreapi;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+//@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum MovieSlot {
-    MORNING,AFTERNOON,EVENING;
+    MORNING("Morning"),AFTERNOON("Afternoon"),EVENING("Evening");
+
+    private final String value;
+
+    MovieSlot(String val) {
+        this.value=val;
+    }
+
+    @JsonValue
+    public String getValue(){
+        return this.value;
+    }
 }
