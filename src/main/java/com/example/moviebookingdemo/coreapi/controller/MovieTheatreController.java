86e10d4c13f254376c01c2a85baeae6af2c75824
sushaanttb <sushantbhalla@ymail.com>
2020-07-13 16:50:10 +0530

    Added CORS support

--- src/main/java/com/example/moviebookingdemo/coreapi/controller/MovieTheatreController.java
+++ src/main/java/com/example/moviebookingdemo/coreapi/controller/MovieTheatreController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/movie-theatre")
 public class MovieTheatreController {
